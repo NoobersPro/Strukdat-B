60e7c04a417c392c59f39c874d4292aa0c3664b5
Naufal Ammar Saputra <102176304+NoobersPro@users.noreply.github.com>
2022-11-15 23:37:32 +0700

    Add files via upload

---
+++ Pertemuan 11-OOP/Soal 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class MHS {
+
+	public:
+	string MHSname;
+	
+	void printname() {
+		cout << "MHS name is : "<< MHSname; 
+	}
+};
+
+int main() {\
+	MHS mhs1;
+
+	mhs1.MHSname = "Nas";
+
+	mhs1.printname();
+	
+	return 0;
+}
