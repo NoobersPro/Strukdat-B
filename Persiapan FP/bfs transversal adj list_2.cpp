bcab4c8e48e4b09b04021a3c66e6bf88217cb42b
Naufal Ammar Saputra <102176304+NoobersPro@users.noreply.github.com>
2022-11-27 21:30:07 +0700

    Update bfs transversal adj list_2.cpp

--- Persiapan FP/bfs transversal adj list_2.cpp
+++ Persiapan FP/bfs transversal adj list_2.cpp
@@ -17,8 +17,8 @@ void bfs(string startVertex, string destination){
 	vector<bool> visited;
 	visited.resize(V,false);
 	queue<pair<int, string>> q;
-	q.push(make_pair(1, startVertex));
-	visited[1] = true;
+	q.push(make_pair(0, startVertex));
+	visited[0] = true;
 	while(!q.empty()){
 		for(auto const &i: adj[q.front().first]){
 			if(!visited[i.first]){
@@ -38,6 +38,7 @@ void bfs(string startVertex, string destination){
 int main(){
 	Graph g(20);
 
+	g.addVertex("Rumah",0, 0);
     g.addVertex("Kos Putri",0, 1);
     g.addVertex("Masjid",0, 11);
     g.addVertex("Rumah Bersalin",0, 12);
@@ -49,15 +50,18 @@ int main(){
     g.addVertex("Bengkel",5, 6);
     g.addVertex("Es Teh Indonesia",6, 7);
     g.addVertex("Depot Nasi Goreng",7, 8);
-    g.addVertex("SPBU",7, 19);
+    g.addVertex("SPBU 2",7, 19);
 	g.addVertex("Masjid",8, 9);
 	g.addVertex("Barber",9, 10);
+	g.addVertex("Barber",10, 10);
 	g.addVertex("Rumah Bersalin",11, 12);
 	g.addVertex("AHASS",12, 13);
 	g.addVertex("SPBU",13, 14);
+	g.addVertex("Hotel",15, 15);
 	g.addVertex("Hotel",16, 15);
 	g.addVertex("SMPN 9",16, 17);
     g.addVertex("SMKN 2",17, 18);
+    g.addVertex("SPBU 2",19, 19);
     
 	cout << "bfs destinations" << endl;
 	g.bfs("Rumah", "Bengkel");
