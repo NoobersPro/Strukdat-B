9583165871d22d564a928359506c690ff978c936
Naufal Ammar Saputra <102176304+NoobersPro@users.noreply.github.com>
2022-11-27 20:09:43 +0700

    Add files via upload

---
+++ bfs  transversal adj matrix_2.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+	int V;
+
+	int adjM[20][20] ={
+          {0, 0},
+          {0, 0}
+    };
+	string place[20];
+	
+public:
+Graph(int v){
+	V = v;
+}
+
+void addVertex(string str, int v1, int v2){
+	adjM[v1][v2] = 1;
+	place[v2] = str;
+}
+
+void bfs(string startVertex, string destination){
+	bool visited[20] = {false};
+	queue<int> q;
+	q.push(1);
+	visited[1] = true;
+	while(!q.empty()){
+		int flag = 0;
+		for(int i = 0; i<= 20; i++){
+			if(!visited[i]){
+				visited[i] = true;
+				q.push(i);
+			}
+		}
+		const char *d = destination.c_str();
+		const char *c = place[q.front()].c_str();
+		cout << place[q.front()] << " ";
+		q.pop();
+		if(strcmp(d, c)==0) break;
+	}
+}
+
+};
+int main(){
+	Graph g(20);
+	g.addVertex("Rumah",0, 0);
+    g.addVertex("Kos Putri",0, 1);
+    g.addVertex("Masjid",0, 11);
+    g.addVertex("Rumah Bersalin",0, 12);
+    g.addVertex("Puskesmas",1, 2);
+    g.addVertex("Hutan Kota",2, 3);
+    g.addVertex("SMPN 8",3, 4);
+    g.addVertex("Indomaret",3, 16);
+    g.addVertex("Warung Bakso",4, 5);
+    g.addVertex("Bengkel",5, 6);
+    g.addVertex("Es Teh Indonesia",6, 7);
+    g.addVertex("Depot Nasi Goreng",7, 8);
+    g.addVertex("SPBU",7, 19);
+	g.addVertex("Masjid",8, 9);
+	g.addVertex("Barber",9, 10);
+	g.addVertex("Rumah Bersalin",11, 12);
+	g.addVertex("AHASS",12, 13);
+	g.addVertex("SPBU 2",13, 14);
+	g.addVertex("Hotel",16, 15);
+	g.addVertex("SMPN 9",16, 17);
+    g.addVertex("SMKN 2",17, 18);
+    
+	cout << "bfs destinations" << endl;
+	g.bfs("Rumah", "Bengkel");
+
+}
---
+++ bfs transversal adj matrix.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+
+
+// function to add edge to the graph
+void addEdge(int x,int y)
+{
+	adj[x][y] = 1;
+	adj[y][x] = 1;
+}
+
+// Function to perform BFS on the graph
+void bfs(int start)
+{
+	// Visited vector to so that
+	// a vertex is not visited more than once
+	// Initializing the vector to false as no
+	// vertex is visited at the beginning
+	vector<bool> visited(adj.size(), false);
+	vector<int> q;
+	q.push_back(start);
+
+	// Set source as visited
+	visited[start] = true;
+
+	int vis;
+	while (!q.empty()) {
+		vis = q[0];
+
+		// Print the current node
+		cout << vis << " ";
+		q.erase(q.begin());
+
+		// For every adjacent vertex to the current vertex
+		for (int i = 0; i < adj[vis].size(); i++) {
+			if (adj[vis][i] == 1 && (!visited[i])) {
+
+				// Push the adjacent node to the queue
+				q.push_back(i);
+
+				// Set
+				visited[i] = true;
+			}
+		}
+	}
+}
+
+int main()
+{
+// number of vertices
+int v = 20;
+
+
+// adjacency matrix
+adj= vector<vector<int>>(v,vector<int>(v,0));
+
+  addEdge(0, 1);
+  addEdge(0, 11);
+  addEdge(0, 12);
+  addEdge(1, 2);
+  addEdge(2, 3);
+  addEdge(3, 4);
+  addEdge(3, 16);
+  addEdge(4, 5);
+  addEdge(5, 6);
+  addEdge(6, 7);
+  addEdge(7, 8);
+  addEdge(7, 19);
+  addEdge(8, 9);
+  addEdge(9, 10);
+  addEdge(11, 12);
+  addEdge(12, 13);
+  addEdge(13, 14);
+  addEdge(16, 15);
+  addEdge(16, 17);
+  addEdge(17, 18);
+    
+
+// perform bfs on the graph
+bfs(0);
+}
